c7f47277dfeaef1d81525f069243231f93358bbc
miagrace06 <mcampanella@umail.ucsb.edu>
2024-11-26 21:33:57 -0800

    Update structs_skeleton.h

--- structs_skeleton.h
+++ structs_skeleton.h
@@ -1,5 +1,16 @@
 // file: structs.h
-// YOUR NAME HERE
+// Name: Mia Campanella
 
 // DECLARE THE struct UndergradStudents HERE:
+struct UndergradStudents {
+  int studentID;   //Student ID number
+  string firstName;  //First name
+  string lastName;   //Last name
+  string major;    //Student's major
+  float yearOneGPA;   //GPA for first year
+  float yearTwoGPA;   //GPA for second year 
+  float yearThreeGPA;   //GPA for third year
+  float yearFourGPA;   //GPA for fourth year
+
+};
 
