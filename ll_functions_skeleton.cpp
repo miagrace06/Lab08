b3f4c41c0c154365a2a59598a34f8348d4ffd324
miagrace06 <mcampanella@umail.ucsb.edu>
2024-11-26 21:31:39 -0800

    Update ll_functions_skeleton.cpp

--- ll_functions_skeleton.cpp
+++ ll_functions_skeleton.cpp
@@ -1,4 +1,9 @@
 // file: ll_functions.cpp
+//Name: Mia Campanella
+#include <iostream>
+#include <string>
+using namespace std;
+
 #include "ll_headers.h"
 
 // Used in the function createLL()
@@ -15,37 +20,133 @@ void h_insert(LinkNodePtr& head, string nom, int num) {
 }
 
 void createLL(LinkNodePtr& h) {
-    // Incomplete function - student must complete
     string nom = "";
-    int num;  
-    while(0) // THIS IS A STUB
-    {
+    int num = 0;
+    //Reset the head pointer to NULL to start fresh
+    h = NULL;
+
+    while (true) {
         cout << "Enter name, then a number. To quit, enter 0 for the name AND 0 for the number:\n";
         cin >> nom >> num;
-        // Incomplete code here...
-        // At some point, you call:    h_insert(h, nom, num);
+
+        //Check for quit condition
+        if (nom == '0' && num == 0) {
+            break;
+    }
+        //Call h_insert to add the node to the front of the list
+        h_insert(h, nom, num);
+    }
+    //If no nodes were added, print the empty list message
+    if (n == NULL) {
+        cout << "This list is empty." << endl;
     }
 }
 
+
+
 void printLL(LinkNodePtr h) {
-    // Incomplete function - student must complete
-    cout << "printLL\n"; // THIS IS A STUB
+    //Check if list is empty
+    if (h == NULL) {
+        cout << "The list is empty." << endl;
+        return;
+    }
+    int nodeCount = 0;
+    LinkNodePtr current = h;
+
+    //Go through the list and print each note 
+    cout << "Printing this list: " << endl;
+    while (current != NULL) {
+        cout << "Node #" << nodeCount << ": " << (*current).name << ", " << (*current.number)) << endl;
+        current = (*current).link;
+        nodeCount++;
+    }
+    cout "--------" << endl;
 }
 
 void insertNodeAfter(LinkNodePtr h) {
-    // Incomplete function - student must complete
-    cout << "insertNodeAfter\n"; // THIS IS A STUB
+    //Check if the list is empty
+    if (h == NULL) {
+        cout << "Cannot insert in a non-existing link." << endl;
+        return;
+    }
+    int position;
+    cout << "enter node position to insert new node after (enter negative number to exit): ";
+    cin >> position;
+
+    //Exit if negative position entered
+    if (position < 0) {
+        return;
+    }
+
+    //Count total number of nodes
+    int nodeCount = 0;
+    LinkNodePtr current = h;
+    while (current != NULL) {
+        nodeCount++;
+        current = (*current).link; //dereference current to get the next node
+    }
+    //Check if position is valid
+    if (position >= nodeCount) {
+        cout << "Position entered is illegal. Nothing inserted." << endl;
+        return;
+    }
+    //Get new node data
+    string nom;
+    int num;
+    cout << "Enter data (name, then number: ";
+    cin >> nom >> num;
+
+    //Find the node at the specified position
+    current = h;
+    for (int i = 0; i < position; i++) {
+        current = (*current).link;
+    }
+    //Create new node
+    LinkNodePtr newNode = new LinkNode;
+    (*newNode).name = nom;
+    (*newNode).number = num;
+
+    //insert the new node
+    (*newNode).link = (*current).link; //Make newNode's link point to the next node
+    (*current).link = newNode; //Update the current node to point to the new node
+        
 }
 
 void findMax(LinkNodePtr h) {
-    // Incomplete function - student must complete
-    int max(0);
+    //Check if the list is empty
+    if (h == NULL) {
+        cout << "This list is empty." << endl;
+        return;
+    }
+    int max = (*h).number; //Dereference h to access the number
+    LinkNodePtr current = (*h).link; //Dereference h to access the next node
+
+    //Go through the list to find maximum
+    while (current != NULL) {
+        if ((*current).number > max) { //Dereference current to access the number
+            max = (*current).number;
+        }
+        current = (*current).link; //Dereference current to move to the next node
+    }
     cout << "Largest number in the list is: " << max << endl;
 }
 
 void findMin(LinkNodePtr h) {
-    // Incomplete function - student must complete
-    int min(0);
+    //Check if list is empty
+    if (h == NULL) {
+        cout << "This list is empty." << endl;
+        return;
+    }
+    int min = (*h).number; //Dereference h to access the number
+    LinkNodePtr current = (*h).link; //Dereference h to access the next node
+
+    //Go through the list to find minimum
+    while (current != NULL) {
+        if ((*current).number < min) { //Derference current to access the number
+            min = (*current).number;
+        }
+        current = (*current).link; //Dereference current to move to the next node
+    }
     cout << "Smallest number in the list is: " << min << endl;
 }
 
